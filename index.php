cfec49b007844d72f2b516ee879ea970862c9a8a
workrav <work.krishna.rav@gmail.com>
2023-04-05 14:43:21 +0530

    changes commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // create an array with data for title, and meta, for each page
 $pgdata = array();
 $pgdata['index'] = array(
- 'title'=>'Title for DEMO SERVER',
+ 'title'=>'Title',
  'description'=>'Here add the description for Home page',
  'keywords'=>'meta keywords, for, home page'
 );
