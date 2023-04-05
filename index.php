9c5fe0d73597fd01ccba582dcf61313f9b8cfac7
workrav <work.krishna.rav@gmail.com>
2023-04-05 07:00:43 +0530

    second commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // create an array with data for title, and meta, for each page
 $pgdata = array();
 $pgdata['index'] = array(
- 'title'=>'Title for Home page',
+ 'title'=>'Title for DEMO JENKINS BUILD PAGE DONE HERE',
  'description'=>'Here add the description for Home page',
  'keywords'=>'meta keywords, for, home page'
 );
